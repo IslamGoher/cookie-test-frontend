1fbccb7a546e6092a5792b06c8ffaf15e4a93f67
IslamGoher <islam.goher@nowpay.com>
2023-08-05 00:31:50 +0300

    Add frontend test files

---
+++ index.js
@@ -0,0 +1,16 @@
+(async function () {
+  let res = await fetch("https://api.alienawy.site/login", {
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify({ data: "some test data" }),
+  });
+  res = await res.json();
+  console.log({ loginRes: res });
+
+  let res2 = await fetch("https://api.alienawy.site/access", {
+    method: "GET",
+    credentials: "include",
+  });
+  res2 = await res2.json();
+  console.log({ accessRes: res });
+})();
