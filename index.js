0ca2931ce319ae5797db26106ec945e3faf05ada
IslamGoher <islam.goher@nowpay.com>
2023-08-05 13:29:29 +0300

    Enhance the experience

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-(async function () {
-  let res = await fetch("https://api.alienawy.site/login", {
+const loginButton = document.querySelector("#login-button");
+const accessButton = document.querySelector("#access-button");
+const headerAccess = document.querySelector("#h3");
+
+loginButton.addEventListener("click", async () => {
+  await login();
+});
+
+accessButton.addEventListener("click", async () => {
+  try {
+    const res = await access();
+    if (res.message.includes("success")) {
+      headerAccess.innerHTML = "success";
+      headerAccess.style.color = 'green';
+    }
+    else
+      headerAccess.innerHTML = "unauthorized";
+      headerAccess.style.color = 'red';
+  } catch (error) {
+    headerAccess.innerHTML = "unauthorized";
+    headerAccess.style.color = 'red';
+  }
+});
+
+async function login() {
+  const res = await fetch("https://api.alienawy.site/login", {
     method: "POST",
     credentials: "include",
     body: JSON.stringify({ data: "some test data" }),
   });
-  res = await res.json();
-  console.log({ loginRes: res });
+  const parsedRes = await res.json();
+  console.log({ loginRes: parsedRes });
+}
 
-  let res2 = await fetch("https://api.alienawy.site/access", {
+async function access() {
+  const res = await fetch("https://api.alienawy.site/access", {
     method: "GET",
     credentials: "include",
   });
-  res2 = await res2.json();
-  console.log({ accessRes: res2 });
-})();
+  const parsedRes = await res.json();
+  return parsedRes;
+}
